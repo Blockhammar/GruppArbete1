73e4546b986a16f71361541c83c60dcdf5171d28
blockkhammar <blockhammar.programmering@gmail.com>
2022-12-07 13:24:41 +0100

    Add project files.

---
+++ GruppArbete1/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using GruppArbete1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GruppArbete1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductsService service;
+        public ProductsController()
+        {
+            service = new ProductsService();
+
+        }
+
+        //Action
+        [HttpGet("")]
+        [HttpGet("index.php")]
+        public IActionResult Index()
+        {
+            var model = service.GetAll();
+            return View(model);
+        }
+    }
+}
---
+++ GruppArbete1/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace GruppArbete1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+    }
+}
