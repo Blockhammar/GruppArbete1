52eaeb369831363e30acfd758bfa996625bf9a61
blockkhammar <blockhammar.programmering@gmail.com>
2022-12-07 14:22:41 +0100

    La till all funktionalitet alla i grupen bidrog

--- GruppArbete1/Controllers/BandsController.cs
+++ GruppArbete1/Controllers/BandsController.cs
@@ -14,10 +14,20 @@ namespace GruppArbete1.Controllers
 
         //Action
         [HttpGet("")]
-        [HttpGet("index.php")]
+        [HttpGet("bands/index")]
         public IActionResult Index()
         {
-            var model = service.GetAll();
+            var model = service.GetAllBands();
+            return View(model);
+        }
+
+
+        //Action
+        [HttpGet("Bands/details/{id}")]
+        public IActionResult Detail(int id)
+        {
+            var modelLista = service.GetAllBands();
+            var model = modelLista.FirstOrDefault(b => b.Id == id);
             return View(model);
         }
     }
--- GruppArbete1/Models/BandsService.cs
+++ GruppArbete1/Models/BandsService.cs
@@ -4,17 +4,24 @@
     {
         List<Band> bandList = new List<Band>
         {
-            new Band { Id = 1, Name = "Creator", Description = "RockBand"},
-            new Band { Id = 2, Name = "Beatles", Description = "Boy Band"},
-            new Band { Id = 3, Name = "Backstreet Boys", Description = "Young band"}
+            new Band { Id = 1, Name = "Kreator", Description = "Kreator is a German thrash metal band from Essen, formed in 1982. Their current lineup consists of lead vocalist and rhythm guitarist Miland \"Mille\" Petrozza, drummer Jürgen \"Ventor\" Reil, lead guitarist Sami Yli-Sirniö, and bassist Frédéric Leclercq. The band's lineup has changed multiple times over its 40-year career, most noticeably with their bassists and lead guitarists."},
+            new Band { Id = 2, Name = "Beatles", Description = "The Beatles were an English rock band, formed in Liverpool in 1960, that comprised John Lennon, Paul McCartney, George Harrison and Ringo Starr. They are regarded as the most influential band of all time[1] and were integral to the development of 1960s counterculture and popular music's recognition as an art form.[2] Rooted in skiffle, beat and 1950s rock 'n' roll, their sound incorporated elements of classical music and traditional pop in innovative ways; the band also explored music styles ranging from folk and Indian music to psychedelia and hard rock. "},
+            new Band { Id = 3, Name = "Backstreet Boys", Description = "The Backstreet Boys (often abbreviated as BSB)[3] is an American vocal group[4] consisting of Nick Carter, Howie Dorough, AJ McLean, and cousins Brian Littrell and Kevin Richardson. Lou Pearlman formed the group in 1993 in Orlando, Florida.\r\n\r\nThe group rose to fame with their international debut album, Backstreet Boys (1996). In the following year, they released their second international album Backstreet's Back (1997), along with their self-titled U.S. debut album, Backstreet Boys (1997), which continued the group's success worldwide. They rose to superstardom with their third studio album Millennium (1999) and its follow-up album, Black & Blue (2000). "}
         };
 
-        public Band[] GetAll()
+        public Band[] GetAllBands()
         {
             return bandList
-                .OrderBy(x => x.Name)
-                .ThenBy(x => x.Description)
+                .OrderBy(x => x.Id)
                 .ToArray();
         }
+
+        public Band GetBandById(int id)
+        {
+            return bandList
+                 .FirstOrDefault(x => x.Id == id);
+        }
+
+
     }
 }
